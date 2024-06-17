5305e13cae62368519287f6c02ec16685297ac29
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-06-17 18:52:04 -0300

    first commit

---
+++ src/main/java/modelly/com/ModellyAPI/ModellyApiApplication.java
@@ -0,0 +1,13 @@
+package modelly.com.ModellyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModellyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModellyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/modelly/com/ModellyAPI/ModellyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package modelly.com.ModellyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModellyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
