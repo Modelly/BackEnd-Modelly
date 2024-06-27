59794e1994781f723fd71a29cc6c3d859e7cdc9d
Matheus R. Morgado <matheusrmorgado@hotmail.com>
2024-06-26 22:29:52 -0300

    Update WebConfig.java

--- api/src/main/java/br/com/modelly/api/config/WebConfig.java
+++ api/src/main/java/br/com/modelly/api/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://plataforma-modelly.vercel.app/")
+                .allowedOrigins("http://localhost:5173", "https://plataforma-modelly.vercel.app/", "https://modelly.online/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
